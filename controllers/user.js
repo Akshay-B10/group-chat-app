4e947269e40f88b5a3148514d09ef8f4705f82c0
Akshay-B10 <akshaybhokare777@gmail.com>
2023-08-02 20:13:04 +0530

    node_modules deleted

--- controllers/user.js
+++ controllers/user.js
@@ -12,9 +12,7 @@ exports.addUser = async (req, res) => {
     try {
         const { name, email, phone, password } = req.body;
         if (email == "" || phone == "" || password == "") {
-            return res.status(400).json({
-                message: "Please fill required details"
-            });
+            throw ("Please fill required details");
         }
         const user = await User.findOne({
             where: {
@@ -22,15 +20,11 @@ exports.addUser = async (req, res) => {
             }
         });
         if (user) {
-            return res.status(400).json({
-                message: "User already exist"
-            });
+            throw ("User already exist");
         }
         const validPhone = parseInt(phone);
         if (phone.length != 10 || !validPhone || validPhone.toString().length != 10) {
-            return res.status(400).json({
-                message: "Invalid mobile number"
-            });
+            throw ("Invalid mobile number");
         }
         const saltRounds = 10;
         const hash = await bcrypt.hash(password, saltRounds);
@@ -45,8 +39,7 @@ exports.addUser = async (req, res) => {
         });
     } catch (err) {
         res.status(400).json({
-            err,
-            message: "Something went wrong"
+            message: err
         });
     }
 };
